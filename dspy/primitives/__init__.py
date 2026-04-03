7a9d278813ff263760f255a11ab732336d9282a1
mango-panther <mango-panther@users.noreply.github.com>
2026-04-03 17:18:09 +0530

    Added tests and run lintter

--- dspy/primitives/__init__.py
+++ dspy/primitives/__init__.py
@@ -1,10 +1,10 @@
 from dspy.primitives.base_module import BaseModule
 from dspy.primitives.code_interpreter import CodeInterpreter, CodeInterpreterError, FinalOutput
+from dspy.primitives.compressor import PromptCompressor
 from dspy.primitives.example import Example
 from dspy.primitives.module import Module
 from dspy.primitives.prediction import Completions, Prediction
 from dspy.primitives.python_interpreter import PythonInterpreter
-from dspy.primitives.compressor import PromptCompressor
 
 __all__ = [
     "BaseModule",
